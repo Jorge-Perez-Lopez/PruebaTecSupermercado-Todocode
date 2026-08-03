40bb32508fba3d12c8c31075271269be8c885672
Jorge Pérez <perezlopezj07@hotmail.com>
2026-08-03 11:07:52 +0200

    Merge Squash: Add Class Modeling DTO (#2)
    Add DTO Classes with its attributes and corresponding annotations

---
+++ PruebaTecSupermercado/src/main/java/com/perezjorge/PruebaTecSupermercado/dto/ProductoDTO.java
@@ -0,0 +1,19 @@
+package com.perezjorge.PruebaTecSupermercado.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ProductoDTO {
+
+    private Long id;
+    private String nombre;
+    private String categoria;
+    private Double precio;
+    private Integer cantidad;
+}
